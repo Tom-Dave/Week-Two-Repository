26bc23354f536f2381bcee8cce4d2590ffe70c1c
Tom Dave <tomjet559@gmail.com>
2026-09-14 17:04:58 +0100

    day one week-two

---
+++ ReadingInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class ReadingInput{
+public static void main(String[] args){
+System.out.print("What is your name? \n");
+Scanner input = new Scanner(System.in);
+String name = input.nextLine().trim();
+System.out.println("Nice to meet you " + name + " !");
+                                      }
+                          }
