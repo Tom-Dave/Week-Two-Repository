f0f8c5836c4c683628aa97e7d11b634d1d2d4ae3
Tom Dave <tomjet559@gmail.com>
2026-09-14 17:01:34 +0100

    day one week-two

---
+++ Casting.java
@@ -0,0 +1,8 @@
+public class Casting{
+public static void main(String[] args){
+String x = "1";
+int y = 65;
+int total = Integer.parseInt(x) + y;
+System.out.println("The total is " + total);
+                                      } 
+                    }
