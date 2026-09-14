2c04f261f646c04be2e0d821ddc9ca4937d28a6d
Tom Dave <tomjet559@gmail.com>
2026-09-14 17:05:59 +0100

    day one week-two

---
+++ MathPower.java
@@ -0,0 +1,8 @@
+public class MathPower{
+public static void main(String[] args) {
+int a = 3;
+int b = 4;
+double result = Math.pow(--a , b);
+System.out.println("The result of 10 divided by 3 is " + result);
+                                      }
+                          }
