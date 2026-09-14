361c541ae26d5ade47c58dcdf38f19df45d9744d
Tom Dave <tomjet559@gmail.com>
2026-09-14 17:05:24 +0100

    day one week-two

---
+++ Random.java
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+public class Random{
+public static void main(String[] args){
+int result = (int)(Math.random() * 100);
+System.out.println(result);
+                                      }
+                  }
