1da94c0867102c071d48f34e9821c9371681a72c
Tom Dave <tomjet559@gmail.com>
2026-09-14 16:54:19 +0100

    Adding Arithmetic file

---
+++ Arithimetic.java
@@ -0,0 +1,7 @@
+public class Arithimetic{
+public static void main(String[] args) {
+double result = (double) 10 / (double) 3;
+result += 2; 
+System.out.println("The result of 10 divided by 3 is " + result);
+                                      }
+                          }
